e5290d99c90de30a5259968c89093b1ee70b0ddc
badbanecat-stack <140128238+badbanecat-stack@users.noreply.github.com>
2026-06-23 11:54:08 -0500

    Add files via upload

--- gpiotest.py
+++ gpiotest.py
@@ -2,7 +2,7 @@ import Jetson.GPIO as gpio
 import time
 
 def gpiotest(stop_event):
-    pin = 31
+    pin_list = [7,29,31,32,33]
 
     #Pin Setup
     gpio.setwarnings(False)
@@ -11,11 +11,14 @@ def gpiotest(stop_event):
     gpio.setmode(gpio.BOARD)
 
     #set pin as an input pin
-    gpio.setup(pin, gpio.OUT, initial = gpio.HIGH)
+    for pin in pin_list:
+        gpio.setup(pin, gpio.OUT)
     while not stop_event.is_set():
-        gpio.output(pin, gpio.LOW)
+        for pin in pin_list:
+            gpio.output(pin, gpio.LOW)
         time.sleep(1)
-        gpio.output(pin, gpio.HIGH)
+        for pin in pin_list:
+            gpio.output(pin, gpio.HIGH)
         time.sleep(1)
 
     gpio.output(pin, gpio.LOW)
--- nanogui.py
+++ nanogui.py
@@ -51,6 +51,8 @@ class NanoGUI():
         self.i2sbutton = tk.Button(self.buttonFrame, text="Test I2S", width=20, height=5)
         self.cambutton = tk.Button(self.buttonFrame, text="Test CAMERA", width=20, height=5)
 
+        
+
         self.gpiobutton.grid(row=0, column=0)
         self.pwmbutton.grid(row=0, column=1)
         self.spibutton.grid(row=0, column=2)
@@ -59,10 +61,37 @@ class NanoGUI():
         self.i2sbutton.grid(row=1, column=2)
         self.cambutton.grid(row=2, column=0)
 
+        self.statusframe = tk.Frame(self.root)
+        self.statusframe.columnconfigure(0, weight=1)
+        self.statusframe.columnconfigure(1, weight=1)
+        self.statuslabel = tk.Label(self.statusframe, text="Status", font=("Comic Sans MS",20))
+        self.gpiolabel = tk.Label(self.statusframe, text="GPIO Status:", font=("Comic Sans MS",16))
+        self.gpiostatus = tk.Label(self.statusframe, text="N/A", font=("Comic Sans MS",16))
+        self.pwmlabel = tk.Label(self.statusframe, text="PWM Status:", font=("Comic Sans MS",16))
+        self.pwmstatus = tk.Label(self.statusframe, text="N/A", font=("Comic Sans MS",16))
+        self.uartlabel = tk.Label(self.statusframe, text="UART Status:", font=("Comic Sans MS",16))
+        self.uartstatus = tk.Label(self.statusframe, text="N/A", font=("Comic Sans MS",16))
+        
+        
+        self.statusframe.place(x=750, y=100)
+        self.statuslabel.grid(row=0, column=1, padx=20,pady=10)
+        self.gpiolabel.grid(row=1, column=0, padx=20, pady=10)
+        self.gpiostatus.grid(row=1, column=1, padx=20, pady=10)
+        self.pwmlabel.grid(row=2, column=0, padx=20, pady=10)
+        self.pwmstatus.grid(row=2, column=1, padx=20, pady=10)
+        self.uartlabel.grid(row=3, column=0, padx=20, pady=10)
+        self.uartstatus.grid(row=3, column=1, padx=20, pady=10)
+
         # widgets for GPIO frame
-        self.gpiolabel = tk.Label(self.gpioFrame, text="The Green LED should be blinking",
+        self.gpiolabel = tk.Label(self.gpioFrame, text="LEDs Start Blinking",
                                   font=("Comic Sans MS", 18))
         self.gpiolabel.pack(padx=20, pady=20)
+        self.gvar = tk.IntVar()
+        self.gpiocheckbox = tk.Checkbutton(self.gpioFrame, text="Are LEDs blinking?", variable = self.gvar)
+        self.gpiocheckbox.pack(padx=10, pady=10)
+        self.warning = tk.Label(self.gpioFrame, text="Make sure to click the checkbox if it works",
+                                  font=("Comic Sans MS", 10), fg="red")
+        self.warning.pack()
         self.gbutton = tk.Button(self.gpioFrame, text="Back", command=self.back_button)
         self.gbutton.pack()
 
@@ -70,6 +99,12 @@ class NanoGUI():
         self.pwmlabel = tk.Label(self.pwmFrame, text="The servo should spin", 
                                  font=("Comic Sans MS", 18))
         self.pwmlabel.pack(padx=20, pady=20)
+        self.pvar = tk.IntVar()
+        self.pwmcheckbox = tk.Checkbutton(self.pwmFrame, text="Is the servo spinning like in the image?", variable = self.pvar)
+        self.pwmcheckbox.pack(padx=10, pady=10)
+        self.warning = tk.Label(self.pwmFrame, text="Make sure to click the checkbox if it works",
+                                  font=("Comic Sans MS", 10), fg="red")
+        self.warning.pack()
         self.pbutton = tk.Button(self.pwmFrame, text="Back", command=self.back_button)
         self.pbutton.pack()
 
@@ -88,12 +123,13 @@ class NanoGUI():
         self.received.pack()
 
         #Load main screen and start the loop
-        self.buttonFrame.pack()
+        self.buttonFrame.place(x=100, y=100)
         self.root.mainloop()
 
     #function for gpio button
     def gpio_test(self):
-            self.buttonFrame.pack_forget()
+            self.buttonFrame.place_forget()
+            self.statusframe.place_forget()
             self.gpioFrame.pack()
 
             self.stop_gpio_test.clear()
@@ -103,47 +139,54 @@ class NanoGUI():
                 daemon=True
             )
             self.gpio_thread.start()
+            
     #function for pwm button
     def pwm_test(self):
-          self.buttonFrame.pack_forget()
-          self.pwmFrame.pack()
+            self.buttonFrame.place_forget()
+            self.statusframe.place_forget()
+            self.pwmFrame.pack()
 
-          self.stop_pwm_test.clear()
-          self.pwm_thread = threading.Thread(
-                target=pwmtest.pwmtest,
-                args=(self.stop_pwm_test,),
-                daemon=True
-          )
+            self.stop_pwm_test.clear()
+            self.pwm_thread = threading.Thread(
+                  target=pwmtest.pwmtest,
+                  args=(self.stop_pwm_test,),
+                  daemon=True
+            )
 
-          self.pwm_thread.start()
+            self.pwm_thread.start()
+            if self.pvar.get() == 1:
+                  self.pwmstatus.config(text="PASS")
+            else:
+                  self.pwmstatus.config(text="FAIL")
 
     def uart_test(self):
-          self.buttonFrame.pack_forget()
+          self.buttonFrame.place_forget()
+          self.statusframe.place_forget()
           self.uartFrame.pack()
 
     def uart_send(self):
-          message = self.uarttextbox.get('1.0')
-          received = uarttest.uarttest(message)
-          self.received.insert('1.0', received)
+            message = self.uarttextbox.get('1.0','end-1c')
+            received = uarttest.uarttest(message)
+            self.received.insert('1.0', received)
+            if message == received:
+                  self.uartstatus.config(text= "PASS")
+            else:
+                  self.uartstatus.config(text= "FAIL")
           
 
     #function for back button
     def back_button(self):
             self.stop_gpio_test.set()
             self.stop_pwm_test.set()
+            
+
+            
             self.pwmFrame.pack_forget()
             self.gpioFrame.pack_forget()
+            self.uarttextbox.delete('1.0','end')
+            self.received.delete('1.0','end')
             self.uartFrame.pack_forget()
-            self.buttonFrame.pack()
-
+            self.statusframe.place(x=750, y=100)
+            self.buttonFrame.place(x=100, y=100)
             
-            
-
-            
-            
-            
-
-    
-
-
 NanoGUI()
